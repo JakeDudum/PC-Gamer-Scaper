a82b051447b819aa7e1d177c29634f044988600e
Jake Dudum <jakedudum@gmail.com>
2019-07-24 18:38:42 -0700

    Removed activity comments and added pathing for using handlebars

--- server.js
+++ server.js
@@ -1,86 +1,64 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Routes
-
-// A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
-  // First, we grab the body of the html with axios
   axios.get("https://www.pcgamer.com/news/").then(function (response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
     $("div.content").each(function (i, element) {
-      // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(element)
         .children("header")
         .children("h3")
         .text();
 
       result.summary = $(element)
-      .children("p")
-      .text()
-      .split("\n")[1];
+        .children("p")
+        .text()
+        .split("\n")[1];
 
       result.link = $(element)
         .parent()
         .parent()
         .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function (dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function (err) {
-          // If an error occurred, log it
           console.log(err);
         });
     });
-
-    // Send a message to the client
     res.send("Scrape Complete");
   });
 });
 
-// Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
-  // TODO: Finish the route so it grabs all of the articles
   db.Article.find({}).then(function (response) {
     res.send(response);
   })
@@ -90,13 +68,7 @@ app.get("/articles", function (req, res) {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-  // TODO
-  // ====
-  // Finish the route so it finds one article using the req.params.id,
-  // and run the populate method with "note",
-  // then responds with the article with the note included
   db.Article.findOne({ _id: mongoose.ObjectId(req.params.id) })
     .populate("note")
     .then(function (response) {
@@ -108,13 +80,7 @@ app.get("/articles/:id", function (req, res) {
     })
 });
 
-// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
   db.Note.create(req.body)
     .then(function (dbNote) {
       return db.Article.findByIdAndUpdate({ _id: mongoose.ObjectId(req.params.id) });
@@ -125,7 +91,10 @@ app.post("/articles/:id", function (req, res) {
     });
 });
 
-// Start the server
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
 app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
